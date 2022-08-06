6f44ccf80ed6de8e781cacc1426bdd9d4c31a6a2
scott-mackenzie19 <70110830+scott-mackenzie19@users.noreply.github.com>
2022-08-06 16:57:18 -0500

    Add files via upload

---
+++ 21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/article.cpp" "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/article.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/catch_setup.cpp" "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/catch_setup.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/main.cpp" "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/main.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/porter2_stemmer.cpp" "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/porter2_stemmer.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/query.cpp" "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/query.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/rwfile.cpp" "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/rwfile.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/tests.cpp" "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/tests.cpp.o"
+  "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/word.cpp" "/mnt/c/Users/dalla/CLionProjects/21f-srch-ngn-kate-scott/CMakeFiles/21f_final_proj_template.dir/word.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
